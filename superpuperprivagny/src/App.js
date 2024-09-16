387e46d938197b214e44c72851e5440c3562b5bf
Dastan2011 <dastandanatov@gmail.com>
2024-09-16 10:46:28 +0600

    first commit

---
+++ superpuperprivagny/src/App.js
@@ -0,0 +1,57 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<header>
+<div className='cont'>
+<div className='div1'>
+<h4 className='shopping'>Hamster Shopping</h4>
+<div className='div2'>
+<button className='reset1' id='reset1'>X</button>
+<h2 className='heart1' id='heart1'>🤍</h2>
+<img className='homiakChek1' src='https://th.bing.com/th/id/OIP.9tUdZx5_BT_YX-RI6jsW5gHaGE?rs=1&pid=ImgDetMain' alt=''></img>
+<span>Олег</span> 
+<span>Цвет:БелоРозовый</span>
+<h4>Сколько хотите?</h4>
+<button className='plus1' id='plus1'>+</button>
+<h2 id='zero1'>0</h2>
+<button className='minus1' id='minus1'>-</button>
+<h2 id='dollar1'>550$</h2>
+</div>
+<div className='div3'>
+<button className='reset2'>X</button>
+<h2 className='heart2' id='heart2'>🤍</h2>
+<img className='homiakChek2' src='https://th.bing.com/th/id/OIP.IJzkGw32eh7-nLuZcL63XAHaEK?rs=1&pid=ImgDetMain' alt=''></img>
+<span>Никита</span>
+<span>Цвет: Белый</span>
+<h4>Сколько хотите?</h4>
+<button className='plus2'>+</button>
+<h2 className='zero2'>0</h2>
+<button className='minus2' id='minus2'>-</button>
+<h2 id='dollar2'>590$</h2>
+</div>
+<div className='div4'>
+<button className='reset3'>X</button>
+<h2 className='heart3' id='heart3'>🤍</h2>
+<img className='homiakChek3' src='https://th.bing.com/th/id/OIP.kR77OccBRLjsq6ZeYplCBQHaFj?rs=1&pid=ImgDetMain' alt=''></img>
+<span>Даня</span>
+<span>Цвет: Желтый</span>
+<h4>Сколько хотите?</h4>
+<button className='plus3' id='plus3'>+</button>
+<h2 id='zero3'>0</h2>
+<button className='minus3' id='minus3'>-</button>
+<h2 id='dollar3'>899$</h2>
+<h5>Очень редкий вид💎</h5>
+</div>
+
+</div>
+
+</div>
+</header>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ superwagnytema/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<header>
+<div className='div1'>
+  
+<div className='div2'>
+
+</div>
+</div>
+</header>
+
+    </div>
+  );
+}
+
+export default App;
